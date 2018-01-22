e6da9ce092c56cc47aade617556b8b8997e70005
tigaloette <35683028+tigaloette@users.noreply.github.com>
2018-01-22 01:13:56 -0800

    Create puzzle 7.js

---
+++ puzzle 7.js
@@ -0,0 +1,36 @@
+while (getColor != "red") {
+  down
+} else {
+  right
+  right
+}
+
+while (getColor != "red") {
+  up
+} else {
+  right
+  right
+}
+
+while (getColor != "red") {
+  down
+} else {
+  right
+  right
+}
+
+while (getColor != "red") {
+  up
+} else {
+  right
+  right
+}
+
+while (getColor != "red") {
+  down
+} else {
+  right
+  right
+}
+
+right
