dde8b0a43d90e675ca67bedb33040aea0f01b19f
tigaloette <35683028+tigaloette@users.noreply.github.com>
2018-01-22 00:45:52 -0800

    Create puzzle 4.js

---
+++ puzzle 4.js
@@ -0,0 +1,27 @@
+Function Repeat (direction, n){
+  Var count = 0
+  While ( count < n)
+  direction
+  count = count + 1
+}
+
+Repeat (down, 4
+Repeat (right, 1）
+
+Var gaaaaaaaate = GetColor
+
+Repeat (down, 1）
+
+SetColor (gaaaaaaaate)
+
+Repeat (right, 2）
+Repeat (up, 1）
+Repeat (right, 1）
+
+Var gaaaaaaaate = GetColor
+
+Repeat (down, 1）
+
+SetColor (gaaaaaaaate)
+
+Repeat (right, 2）
