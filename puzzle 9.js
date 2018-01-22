8c940d17cdb6fa3917621ec0f27c61bb188f58e2
tigaloette <35683028+tigaloette@users.noreply.github.com>
2018-01-22 01:23:20 -0800

    Create puzzle 9.js

---
+++ puzzle 9.js
@@ -0,0 +1,32 @@
+down
+down
+right
+right
+
+Var count = 0
+While ( count != 5){
+  set color (rainbow)
+  right
+  count = count + 1
+}
+
+Var count = 0
+While ( count != 2){
+  set color (rainbow)
+  down
+  count = count + 1
+}
+
+Var count = 0
+While ( count != 4){
+  set color (rainbow)
+  left
+  count = count + 1
+}
+
+Var count = 0
+While ( count != 2){
+  set color (rainbow)
+  up
+  count = count + 1
+}
