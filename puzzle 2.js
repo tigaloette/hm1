fe4dfd73dced8b40262082c3682e86ffeb08f965
tigaloette <35683028+tigaloette@users.noreply.github.com>
2018-01-22 00:36:36 -0800

    Create puzzle 2.js

---
+++ puzzle 2.js
@@ -0,0 +1,13 @@
+Function Repeat (direction, n){
+  Var count = 0
+  While ( count < n)
+  direction
+  count = count + 1
+}
+
+Repeat (down, 6）
+Repeat (right, 3)
+Repeat (up, 2)
+Repeat (right, 2)
+Repeat (down, 1)
+Repeat (down, 2)
