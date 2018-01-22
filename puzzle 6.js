e845e762a2f40ca070a156f28a54caed1ba4ae63
tigaloette <35683028+tigaloette@users.noreply.github.com>
2018-01-22 01:11:42 -0800

    Create puzzle 6.js

---
+++ puzzle 6.js
@@ -0,0 +1,24 @@
+right
+
+Var LastDirection = GetColor
+
+if (getColor == "red") {
+  up
+  up
+  up
+} else {
+  down
+  down
+  down
+  
+if (getColor == "red") {
+  left
+} else {
+  right
+  
+SetColor (LastDirection)
+
+if (getColor == "red") {
+  up
+} else {
+  down
